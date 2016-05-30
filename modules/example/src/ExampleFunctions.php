f52bc0519b2178b96c0d5c51c3fb8ce300aafa58
Erlend ter Maat <erwitema@gmail.com>
2016-05-30 11:08:41 +0200

    Explain the context of this test module

--- modules/example/src/ExampleFunctions.php
+++ modules/example/src/ExampleFunctions.php
@@ -4,6 +4,9 @@ namespace Drupal\stub_example;
 
 use Drupal\drupalstub\Functions;
 
+/**
+ * Allow the API of this module to be used in a decoupled environment
+ */
 class ExampleFunctions extends Functions {
 
   public function add_a_numer_to_something_random($random, $add) {
