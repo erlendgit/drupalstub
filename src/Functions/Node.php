f04274835206c462859dd75405a482f1364611e8
Erlend ter Maat <erwitema@gmail.com>
2016-05-30 09:27:15 +0200

    Implement common global functions in User and Node

--- src/Functions/Node.php
+++ src/Functions/Node.php
@@ -1,8 +1,25 @@
 <?php
 
 namespace Drupal\drupalstub\Functions;
+
 use Drupal\drupalstub\Functions;
 
 class Node extends Functions {
+
+  public function node_load($nid, $vid = NULL, $reset = FALSE) {
+    return node_load($nid, $vid, $reset);
+  }
+
+  public function node_save($node) {
+    return node_save($node);
+  }
+
+  public function node_object_prepare($node) {
+    return node_object_prepare($node);
+  }
   
+  public function node_access($op, $node, $account=NULL) {
+    return node_access($op, $node, $account);
+  }
+
 }
--- src/Functions/User.php
+++ src/Functions/User.php
@@ -1,8 +1,21 @@
 <?php
 
 namespace Drupal\drupalstub\Functions;
+
 use Drupal\drupalstub\Functions;
 
 class User extends Functions {
-  
+
+  public function user_load($uid, $reset = FALSE) {
+    return user_load($uid, $reset);
+  }
+
+  public function user_save($account, $edit = array(), $category = 'account') {
+    return user_save($account, $edit, $category);
+  }
+
+  public function user_login_finalize(&$edit = array()) {
+    return user_login_finalize($edit);
+  }
+
 }
